7fc15fc0de4c0d0b11fb899e86af7d7ebd2f976b
techtobit <nurthern2016@gmail.com>
2024-07-13 14:53:18 +0600

    removed old icon and added new icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import CandlestickChart from './CandlestickChart';
 import './App.css';
+import candleIcon from './assets/candlestick.png'
+import lineIcon from './assets/lineChart.png'
+import lineAreaIcon from './assets/lineArea.png'
 
 const App: React.FC = () => {
   const symbols = ['IBM', 'MSFT', 'MICROSOFT',];
@@ -37,12 +40,13 @@ const App: React.FC = () => {
         </div>
         <div className='border-2 '>
           <button className='px-4 btn btn-blue' onClick={() => handleCartTypeClick('candlestick')}>
-            CandelStick
+            <img className='w-8 h-8' src={candleIcon} alt="" />
           </button>
           <button className='px-4 btn btn-blue'  onClick={() => handleCartTypeClick('line')}>
-            Line
+            <img className='w-8 h-8' src={lineIcon} alt="" />
           </button>
-          <button  className='px-4 btn btn-blue'  onClick={() => handleCartTypeClick('area')}> Area
+          <button  className='px-4 btn btn-blue'  onClick={() => handleCartTypeClick('area')}>
+            <img className='px-4 btn btn-blue'  src={lineAreaIcon} alt="" />
           </button>
 
         </div>
